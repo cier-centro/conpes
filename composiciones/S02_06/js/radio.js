812778c79bd6145b6e164ee938e0f4f15cbbba22
seval931 <svalencias@unal.edu.co>
2017-11-07 16:03:07 -0500

    correction validator

--- composiciones/S02_06/js/radio.js
+++ composiciones/S02_06/js/radio.js
@@ -148,16 +148,16 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // los alias más comu
 			sym.$("InputA_"+i).children("input").each(function(){
 			ValorTemp = sym.getVariable("Respuesta_"+i);
 			if($(this).is(":checked")){
-			/*if(ValorTemp==1){
+			if(ValorTemp==1){
 			//console.log("esta chequeado "+($(this).attr("id"))+" y el valor temporal es "+ValorTemp+" correcto");;
-			sym.$("ok"+i).css({top: y, left: x});
-			sym.$("ok"+i).show();
+			//sym.$("ok"+i).css({top: y, left: x});
+			//sym.$("ok"+i).show();
 			}else{
 			//console.log("esta chequeado "+($(this).attr("id"))+" y el valor temporal es "+ValorTemp+" incorrecto");
 			errores=errores+1
-			sym.$("bad"+i).css({top: y, left: x});
-			sym.$("bad"+i).show();
-			}*/
+			//sym.$("bad"+i).css({top: y, left: x});
+			//sym.$("bad"+i).show();
+			}
 			}else{
 			var radioBCheck= $("#radioB_"+i).is(":checked");
 			if (radioBCheck){
